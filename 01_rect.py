d0c37dc8b4f4ddf9936b4d0f1b7109268b05d34c
M. Fadhil Mahendra <mfmahendr@gmail.com>
2026-03-30 13:52:41 +0700

    feat: :sparkles: add positional argument

--- 01_rect.py
+++ 01_rect.py
@@ -1,12 +1,33 @@
 import pygame
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# input
+rect_size = 100
+n = 3
+line_width = 10
+
+parser.add_argument("size", help="rectangular size to draw")
+parser.add_argument("width", help="rectangular line width to draw")
+parser.add_argument("n", help="number of rectangular to draw")
+args = parser.parse_args()
+if args.n:
+    n = int(args.n)
+if args.size:
+    rect_size = int(args.size)
+if args.width:
+    line_width = int(args.width)
 
 pygame.init()
-screenSize = [720, 720]
+total_rect_width = (rect_size+line_width)*n 
+if (total_rect_width > 1080):
+    total_rect_width = 1080
+
+screenSize = [total_rect_width, total_rect_width]
 screen = pygame.display.set_mode(screenSize)
 color = pygame.Color(0, 0, 255)
 clock = pygame.time.Clock()
-
-rect_size = 100
 
 done = False
 while not done:
@@ -20,8 +41,8 @@ while not done:
 
     posX = 0
     posY = 0
-    for i in range (3):
-        pygame.draw.rect(screen, color, [posX, posY, rect_size, rect_size], 10)
+    for i in range (n):
+        pygame.draw.rect(screen, color, [posX, posY, rect_size, rect_size], line_width)
         posX += rect_size
         posY += rect_size
     pygame.display.flip()
